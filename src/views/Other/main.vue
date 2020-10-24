2de44ad7e893d1dc60165f7fab5da1135e3b93e4
piggy1024 <1293266846@qq.com>
2020-10-24 09:44:21 +0800

    兄弟间组件通信

---
+++ src/views/Other/main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- 父组件 -->
+    <p>请输入单价: <input type="text" v-model="price" /></p>
+    <!-- 子组件1 -->
+    <page1 :price="price" @downPrice="downPrice"></page1>
+    <!-- 子组件2 -->
+    <page2></page2>
+  </div>
+</template>
+
+<script>
+import Page1 from "./PageOne";
+import Page2 from "./PageTwo";
+export default {
+  name: "App",
+  data() {
+    return {
+      price: ""
+    };
+  },
+  components: {
+    Page1,
+    Page2
+  },
+  methods: {
+    downPrice() {
+      this.price = (this.price - 1).toString();
+      console.log("highlight");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/gisMap/gisMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>wantedList内容:{{ wantedList }}</div>
+</template>
+
+<script>
+import _ from "underscore";
+export default {
+  data() {
+    return {
+      list: [
+        { name: "张三", age: 19 },
+        { name: "李四", age: 19 }
+      ],
+      wantedList: []
+    };
+  },
+  created() {
+    this.getWantedObject();
+  },
+  methods: {
+    getWantedObject() {
+      this.wantedList = _.where(this.list, { name: "张三" });
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped></style>
